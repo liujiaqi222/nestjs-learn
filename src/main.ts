b2a322c7f23b302eab188e20108307f357747ba9
liujiaqi222 <2473487465@qq.com>
2025-04-18 15:09:51 +0800

    再次重试部署vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  const host = '0.0.0.0';
-  await app.listen(port, host);
+  await app.listen(port);
 }
 bootstrap();
